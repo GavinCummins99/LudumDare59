7297a1c432685c4912ed518e5f9475dbd1d0c2b9
Pallidinn <rianlambe@icloud.com>
2026-04-18 21:21:47 +0100

    Merge with gav

--- Assets/Environment/Button/Button.cs
+++ Assets/Environment/Button/Button.cs
@@ -6,23 +6,12 @@ public class Button : MonoBehaviour
 {
     public UnityEvent OnPressed;
     public UnityEvent OnReleased;
-    public bool HeavyWeight = false;
-    public float WeightThreshold = 2f;
 
-    private List<Player2D> _playersOnPlate = new List<Player2D>();
-    private bool _isPressed = false;
-
-    //Call in editor 
-    void OnValidate() {
-        GetComponentInChildren<SpriteRenderer>().color = HeavyWeight ? Color.red : Color.white;
-    }
 
     //Called when other character enters box trigger
     void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player") || other.CompareTag("Picmin")) {
             Player2D p2d = other.GetComponent<Player2D>();
-            if (p2d != null) _playersOnPlate.Add(p2d);
-            CheckPlate();
         }
     }
 
@@ -30,28 +19,6 @@ public class Button : MonoBehaviour
     void OnTriggerExit2D(Collider2D other) {
         if (other.CompareTag("Player") || other.CompareTag("Picmin")) {
             Player2D p2d = other.GetComponent<Player2D>();
-            if (p2d != null) _playersOnPlate.Remove(p2d);
-            CheckPlate();
-        }
-    }
-
-    //Check if it should be pressed or not, and invoke events accordingly
-    void CheckPlate() {
-        float totalWeight = 0f;
-        foreach (Player2D player in _playersOnPlate)
-            totalWeight += player.Weight;
-
-        bool shouldPress = HeavyWeight ? totalWeight >= WeightThreshold : _playersOnPlate.Count > 0;
-
-        if (shouldPress && !_isPressed) {
-            _isPressed = true;
-            GetComponentInChildren<SpriteRenderer>().transform.localScale = new Vector3(1, .1f, 1);
-            OnPressed.Invoke();
-        }
-        else if (!shouldPress && _isPressed) {
-            _isPressed = false;
-            GetComponentInChildren<SpriteRenderer>().transform.localScale = new Vector3(1, 1f, 1);
-            OnReleased.Invoke();
         }
     }
 }
