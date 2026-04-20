d3372c5ae64a05f6150bf9a4f12e19b4a9cad680
Pallidinn <rianlambe@icloud.com>
2026-04-20 23:31:45 +0100

    Captain button press

--- Assets/Environment/Button/Button.cs
+++ Assets/Environment/Button/Button.cs
@@ -1,24 +1,47 @@
 using UnityEngine;
 using UnityEngine.Events;
 using System.Collections.Generic;
+using System.Collections;
 
 public class Button : MonoBehaviour
 {
-    public UnityEvent OnPressed;
-    public UnityEvent OnReleased;
+    public bool Invert = false;
+    float PositionOffset = -2;
+    public UnityEvent ButtonPressed;
 
-
-    //Called when other character enters box trigger
-    void OnTriggerEnter2D(Collider2D other) {
-        if (other.CompareTag("Player") || other.CompareTag("Picmin")) {
-            Player2D p2d = other.GetComponent<Player2D>();
-        }
+    private void OnValidate()
+    {
+        gameObject.transform.localScale = Invert ? new Vector3(-1, 1, 1) : Vector3.one;
     }
 
-    //Called when other character enters box trigger
-    void OnTriggerExit2D(Collider2D other) {
-        if (other.CompareTag("Player") || other.CompareTag("Picmin")) {
-            Player2D p2d = other.GetComponent<Player2D>();
+    public void UsePipe()
+    {
+        StartCoroutine(PipeSequence());
+    }
+
+    private IEnumerator PipeSequence()
+    {
+        GameObject Captain = GameObject.Find("Captain");
+        if (Captain == null) yield break;
+
+        Player2D Player = Captain.GetComponent<Player2D>();
+
+        // Walk to button entrance and wait until arrived
+        if (Player != null)
+        {
+            Player.WalkToPoint(transform.position, 1.8f);
+            yield return new WaitUntil(() => !Player.IsWalking);
         }
+
+        // Play animation and wait 1.5s
+        Animator Anim = Captain.GetComponentInChildren<Animator>();
+        if (Anim != null)
+            Anim.SetTrigger("Special");
+
+        yield return new WaitForSeconds(.5f);
+
+        ButtonPressed.Invoke();
+
+
     }
 }
