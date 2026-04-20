81879b0df9860f3dd9205f75e617d3c3ac0df0cf
Gavin Cummins <101557298+GavinCummins99@users.noreply.github.com>
2026-04-20 23:05:07 +0100

    Added battery pickup to end level and load new level

--- Assets/Player/LevelTrigger.cs
+++ Assets/Player/LevelTrigger.cs
@@ -3,21 +3,17 @@ using UnityEngine;
 public class LevelTrigger : MonoBehaviour
 {
     public string levelName = null;
-   
     
     //loads level on trigger enter
-    void OnTriggerEnter2D(Collider2D other)
+    void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<BoxCollider2D>().enabled = false;
             GetComponent<SceneTransition>().WipeOut(levelName);
             Debug.Log("WIPEOUT");
         }
     }
-
-    // Update is called once per frame
-    void OnTriggerExit2D(Collider2D other)
-    {
-        
-    }
+    
 }
