08b24c3996b2805b8995e7e00930bb4cd298374e
Pallidinn <rianlambe@icloud.com>
2026-04-21 01:01:32 +0100

    Merged with tutorial

--- Assets/Player/ButtonHover.cs
+++ Assets/Player/ButtonHover.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TMPButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    int index = 0;
+    public Sprite[] sprites;
+    public Image spriteRenderer;
+    public Sprite Next;
+
+
     private Animator animator;
 
     void Awake()
@@ -14,18 +21,34 @@ public class TMPButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandle
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        animator.enabled = true;
+        //animator.enabled = true;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        animator.enabled = false;
+        //animator.enabled = false;
     }
     
     public void LoadFirstLevel()
     {
         int currentIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentIndex + 1);
+    }
+
+    public void Tutorial()
+    {
+
+        if (index == 9)
+        {
+            LoadFirstLevel();
+        }
+
+        GetComponent<Image>().sprite = Next;
+        GetComponent<RectTransform>().anchoredPosition = new Vector2(300, -135);
+        spriteRenderer.sprite = sprites[index];
+        index++;
+
+
     }
 
     public void QuitGame()
